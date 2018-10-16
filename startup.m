a18e882fefd9b3fbed3aa835e68548f171cce048
skannerdarkly <skanner@principlepowerinc.com>
2018-10-16 08:55:45 -0700

    Changed path to add to MATLAB

--- startup.m
+++ startup.m
@@ -1,4 +1,4 @@
-% We just added this file!
-addpath('C:\Program Files (x86)\Orcina\OrcaFlex\10.1\OrcFxAPI\MATLAB')
+% Now we just changed this file!
+addpath('C:\foo\bar')
 mydir=userpath;
 addpath(genpath([mydir(1:end-1) filesep]));
